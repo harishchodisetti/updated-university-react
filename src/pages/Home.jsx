eb74a85782783747bb3b7bfd2913d425f579e8bf
harishchodisetti <harishchodisetti@gmail.com>
2026-03-16 15:43:38 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+// import React from 'react'
+// // import Navbar from '../components/Navbar/Navbar'
+// import Hero from '../components/Hero/Hero'
+// import Program from '../components/program/program'
+// import Programs from '../components/programs/programs'
+// import About from '../components/about/About'
+// import Campus from '../components/campus/campus'
+// import Campusphoto from '../components/campusphotos/campusphoto'
+// import Contact from '../components/contact/contact'
+// import Testimonial from '../components/testimonials/testimonials'
+// import Title from '../components/hcontact/hcontact'
+
 import React from 'react'
-// import Navbar from '../components/Navbar/Navbar'
+
 import Hero from '../components/Hero/Hero'
 import Program from '../components/program/program'
 import Programs from '../components/programs/programs'
